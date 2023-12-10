2bf0fb0fd744d60048cd67b16ced9322dc6adbae
Vanessa Ponton <vanessa.ponton@gmail.com>
2023-12-10 09:06:08 +0100

    components.2

--- src/assets/PokemonCard.jsx
+++ src/assets/PokemonCard.jsx
@@ -1,14 +1,30 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 function PokemonCard() {
+    const pokemon = pokemonList[0];
     return (
-
         <figure>
-            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'></img>
-            <figcaption >Bulbasaur</figcaption>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+            <figcaption >{pokemon.name}</figcaption>
         </figure>
 
     );
 }
 
-//export PokemonCard from "./components/PokemonCard";
+
 
 export default PokemonCard;
+
+
+
+
+
+
